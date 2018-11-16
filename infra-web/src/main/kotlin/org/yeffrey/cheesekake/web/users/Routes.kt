25c307f6eb7f78481775dfbc117dfb014deb3cbc
YeFFreY <johan.mslms@outlook.com>
2018-11-16 19:00:20 +0100

    updating routing to use Location everywhere

--- infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/users/Routes.kt
+++ infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/users/Routes.kt
@@ -4,12 +4,12 @@ import io.ktor.application.call
 import io.ktor.auth.authenticate
 import io.ktor.auth.principal
 import io.ktor.html.respondHtml
+import io.ktor.locations.Location
+import io.ktor.locations.get
+import io.ktor.locations.post
 import io.ktor.request.receive
 import io.ktor.response.respondRedirect
 import io.ktor.routing.Route
-import io.ktor.routing.get
-import io.ktor.routing.post
-import io.ktor.routing.route
 import io.ktor.sessions.sessions
 import io.ktor.sessions.set
 import kotlinx.html.*
@@ -17,53 +17,54 @@ import org.yeffrey.cheesekake.api.usecase.users.RegisterUser
 import org.yeffrey.cheesekake.web.CheeseKakeSesion
 import org.yeffrey.cheesekake.web.CheesePrincipal
 
+class Users {
+    @Location("/users/newSession")
+    class NewSession
+
+    @Location("/users/registration")
+    class Registration
+}
+
 fun Route.users(registerUser: RegisterUser) {
-    route("/users") {
-
-        route("/login") {
-            get {
-                call.respondHtml {
-                    head {
-                        title { +"Cheesekake" }
+    get<Users.NewSession> {
+        call.respondHtml {
+            head {
+                title { +"Cheesekake" }
+            }
+            body {
+                h1 {
+                    +"Login"
+                }
+                form(encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
+                    label {
+                        +"Username"
+                        textInput {
+                            name = "user"
+                        }
                     }
-                    body {
-                        h1 {
-                            +"Login"
+                    label {
+                        +"Password"
+                        passwordInput {
+                            name = "password"
                         }
-                        form(encType = FormEncType.applicationXWwwFormUrlEncoded, method = FormMethod.post) {
-                            label {
-                                +"Username"
-                                textInput {
-                                    name = "user"
-                                }
-                            }
-                            label {
-                                +"Password"
-                                passwordInput {
-                                    name = "password"
-                                }
-                            }
-                            submitInput() {
-                                value = "Login"
-                            }
-                        }
+                    }
+                    submitInput() {
+                        value = "Login"
                     }
                 }
             }
-            authenticate("login") {
-                post {
-                    val principal = call.principal<CheesePrincipal>()
-                    call.sessions.set(CheeseKakeSesion(principal!!.userId))
-                    call.respondRedirect("/", false)
-                }
-            }
         }
+    }
+    authenticate("login") {
+        post<Users.NewSession> {
+            val principal = call.principal<CheesePrincipal>()
+            call.sessions.set(CheeseKakeSesion(principal!!.userId))
+            call.respondRedirect("/", false)
+        }
+    }
 
-        route("/registration") {
-            post {
-                val registration = call.receive<RegistrationDto>()
-                registerUser.handle(registration.toRequest(), RegistrationPresenter(call))
-            }
-        }
+    post<Users.Registration> {
+        val registration = call.receive<RegistrationDto>()
+        registerUser.handle(registration.toRequest(), RegistrationPresenter(call))
     }
 }
