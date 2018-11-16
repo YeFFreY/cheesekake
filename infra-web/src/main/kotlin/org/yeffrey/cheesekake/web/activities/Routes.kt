c5ec52fb8ef6abba9f046bf9b676f9de1669c25b
YeFFreY <johan.mslms@outlook.com>
2018-11-16 18:59:04 +0100

    updating routing to use Location everywhere

--- infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/activities/Routes.kt
+++ infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/activities/Routes.kt
@@ -13,31 +13,32 @@ import org.yeffrey.cheesekake.api.usecase.activities.*
 import org.yeffrey.cheesekake.web.WebAction
 import org.yeffrey.cheesekake.web.withPrincipalId
 
-@Location("/activities")
-class ActivitiesSummary
+class Activities {
+    @Location("/activities")
+    class ActivitiesSummary
 
-@Location("/activities/{activityId}")
-data class ActivityDetails(val activityId: Int)
+    @Location("/activities/{activityId}")
+    data class ActivityDetails(val activityId: Int)
 
-@Location("/activities/creation")
-class ActivityCreation
+    @Location("/activities/creation")
+    class ActivityCreation
 
-@Location("/activities/{activityId}/correction")
-data class ActivityCorrection(val activityId: Int)
+    @Location("/activities/{activityId}/correction")
+    data class ActivityCorrection(val activityId: Int)
 
-@Location("/activities/{activityId}/resourceAddition")
-data class ResourceAddition(val activityId: Int)
-
+    @Location("/activities/{activityId}/resourceAddition")
+    data class ResourceAddition(val activityId: Int)
+}
 class ActivitiesRoutes {
     companion object {
         fun hrefs(resource: Resource, call: ApplicationCall): List<WebAction> {
             var links: MutableList<WebAction> = mutableListOf()
             resource.actions.map { action ->
 
-                links.add(WebAction("activity:self", call.application.locations.href(ActivityDetails(resource.id))))
+                links.add(WebAction("activity:self", call.application.locations.href(Activities.ActivityDetails(resource.id))))
                 if (action.name == "editable") {
-                    links.add(WebAction("activity:details:correction", call.application.locations.href(ActivityCorrection(resource.id))))
-                    links.add(WebAction("activity:resources:addition", call.application.locations.href(ResourceAddition(resource.id))))
+                    links.add(WebAction("activity:details:correction", call.application.locations.href(Activities.ActivityCorrection(resource.id))))
+                    links.add(WebAction("activity:resources:addition", call.application.locations.href(Activities.ResourceAddition(resource.id))))
                 }
             }
             return links.toList()
@@ -48,24 +49,24 @@ class ActivitiesRoutes {
 
 
 fun Route.activities(createActivity: CreateActivity, updateActivity: UpdateActivity, queryActivities: QueryActivities, getActivityDetails: GetActivityDetails, addResource: AddResource) {
-    get<ActivitiesSummary> {
+    get<Activities.ActivitiesSummary> {
         val queryTitleContains = call.request.queryParameters["titleContains"]
         queryActivities.handle(QueryActivities.Request(queryTitleContains), QueryActivitiesPresenter(call), withPrincipalId(call))
     }
-    get<ActivityDetails> { details ->
+    get<Activities.ActivityDetails> { details ->
         getActivityDetails.handle(GetActivityDetails.Request(details.activityId), GetActivityDetailsPresenter(call), withPrincipalId(call))
     }
 
-    post<ActivityCreation> {
+    post<Activities.ActivityCreation> {
         val input = call.receive<CreateActivityDto>()
         createActivity.handle(input.toRequest(), CreateActivityPresenter(call), withPrincipalId(call))
     }
-    post<ActivityCorrection> { detailsCorrection ->
+    post<Activities.ActivityCorrection> { detailsCorrection ->
         val input = call.receive<UpdateActivityDto>()
         updateActivity.handle(input.toRequest(detailsCorrection.activityId), UpdateActivityPresenter(call), withPrincipalId(call))
     }
 
-    post<ResourceAddition> { resourceAddition ->
+    post<Activities.ResourceAddition> { resourceAddition ->
         val input = call.receive<AddResourceDto>()
         addResource.handle(input.toRequest(resourceAddition.activityId), AddResourcePresenter(call), withPrincipalId(call))
     }
