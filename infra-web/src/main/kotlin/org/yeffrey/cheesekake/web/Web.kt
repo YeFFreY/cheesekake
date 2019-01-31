bf64072072c2f357b0bfbe1b22a87f4b03ab33ee
YeFFreY <johan.mslms@outlook.com>
2019-01-31 16:06:47 +0100

    using jackson mapper to get graphql request input

--- infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/Web.kt
+++ infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/Web.kt
@@ -1,7 +1,13 @@
 package org.yeffrey.cheesekake.web
 
 import arrow.core.Option
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.yeffrey.cheesekake.api.usecase.Principal
 import org.yeffrey.cheesekake.api.usecase.UseCaseContext
 
 class WebContext<R>(override val request: R, override val principal: Option<Principal> = Option.empty()) : UseCaseContext<R>
+object JsonMapper {
+    private val mapper = jacksonObjectMapper()
+
+    fun <T> mapTo(arguments: Map<String, Any>, toValueType: Class<T>): T = mapper.convertValue(arguments, toValueType)
+}
--- infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/api/activities/Mutations.kt
+++ infra-web/src/main/kotlin/org/yeffrey/cheesekake/web/api/activities/Mutations.kt
@@ -1,25 +1,20 @@
 package org.yeffrey.cheesekake.web.api.activities
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import graphql.schema.idl.TypeRuntimeWiring
 import org.yeffrey.cheesekake.api.usecase.activities.ActivityDto
 import org.yeffrey.cheesekake.api.usecase.activities.CreateActivity
 import org.yeffrey.cheesekake.api.usecase.activities.UpdateActivityGeneralInformation
+import org.yeffrey.cheesekake.web.JsonMapper
 import org.yeffrey.cheesekake.web.WebContext
 import org.yeffrey.cheesekake.web.core.filter.Session
 import org.yeffrey.cheesekake.web.fetcherPresenter
 
 private fun toCreateActivityRequest(arguments: Map<String, Any>): CreateActivity.Request {
-    val categoryId = arguments["categoryId"] as Int
-    val title = arguments["title"] as String
-    val summaryFormatted = arguments["summaryFormatted"] as String
-    val summaryRaw = arguments["summaryRaw"] as String
-    return CreateActivity.Request(categoryId, title, summaryFormatted, summaryRaw)
+    return JsonMapper.mapTo(arguments, CreateActivity.Request::class.java)
 }
 
 private fun toUpdateActivityGeneralInformationRequest(arguments: Map<String, Any>): UpdateActivityGeneralInformation.Request {
-    val mapper = jacksonObjectMapper()
-    return mapper.convertValue(arguments, UpdateActivityGeneralInformation.Request::class.java)
+    return JsonMapper.mapTo(arguments, UpdateActivityGeneralInformation.Request::class.java)
 }
 
 fun TypeRuntimeWiring.Builder.activityMutations(createActivity: CreateActivity, updateActivityGeneralInformation: UpdateActivityGeneralInformation) {
